550044ce7afc99544e1e6f19d2d64ee51f48ba28
Programmizd 02 <programmizd02@gmail.com>
2024-01-12 22:48:25 +0100

    - небольшие исправления в переносе ссылок в цитатах

--- forum/skins/default/desktop/moderation_log.php
+++ forum/skins/default/desktop/moderation_log.php
@@ -174,10 +174,13 @@ $selected = (val_or_empty($_SESSION["moderator_log_filter"]["forum"]) == $fid) ?
 <option value="<?php echo_html($fid); ?>" <?php echo($selected); ?>><?php echo_html($fdata["name"]); ?></option>
 <?php endforeach; ?>
 
+<?php if($fmanager->is_logged_in() && !$fmanager->is_master_admin()): ?>
 <?php
 $selected = (val_or_empty($_SESSION["moderator_log_filter"]["forum"]) == "private") ? "selected" : "";
 ?>
 <option value="private" <?php echo($selected); ?>><?php echo_html(text("PrivateTopics")); ?></option>
+<?php endif; ?>
+
 </select>
 </td>
 <td>
