b8f0710f5661962b091f43126807f285f95f99a3
programmizd02 <137833587+programmizd02@users.noreply.github.com>
2024-01-14 21:15:14 +0100

    Update main_menu_inc.php

--- forum/skins/default/desktop/main_menu_inc.php
+++ forum/skins/default/desktop/main_menu_inc.php
@@ -42,7 +42,7 @@ else
 
 echo($member_link); 
 
-if ($fmanager->get_last_posted_user_name() != $fmanager->get_user_name()) {
+if ($fmanager->get_last_posted_user_name() != $fmanager->get_user_name() && !empty($_SESSION["guest_posting_mode"])) {
   $aname_appendix = "&aname=" . System::generateHash($READ_MARKER . $fmanager->get_last_posted_user_name(), SALT_KEY);
 
   echo " / <a class='guest_link' href='view_guest_profile.php?guest=" . xrawurlencode($fmanager->get_last_posted_user_name()) . $aname_appendix . "'>" . escape_html($fmanager->get_last_posted_user_name()) . "</a>";
