b50840e0d536857ce302da23342d96323d259d3d
Anton Koudinov <anton@koudinov.ru>
2024-02-16 14:23:04 +0300

    Edit custorm_head, custom_body

--- forum/skins/default/desktop/custom_body.php
+++ forum/skins/default/desktop/custom_body.php
@@ -1,1 +1,5 @@
 <!-- write your adjustments here -->
+
+<!-- Yandex.Metrika counter -->
+<noscript><div><img src="https://mc.yandex.ru/watch/0123456789" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+<!-- /Yandex.Metrika counter -->
