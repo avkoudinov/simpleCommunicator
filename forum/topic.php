d12c0d9b756e3e57971a755ff73ff71c3988b020
Programmizd 02 <programmizd02@gmail.com>
2024-02-27 09:27:06 +0100

    исправлен баг в отображении ондайн юзеров при поиске в теме

--- forum/topic.php
+++ forum/topic.php
@@ -418,7 +418,6 @@ $forum_readers = array();
 $topic_readers = array();
 $topic_ignorers = array();
 $topic_blocked_users = array();
-$topic_blocked_users = array();
 $fmanager->get_online_users($online_users, $forum_readers, $topic_readers, $topic_ignorers, $topic_blocked_users, $fid, $tid);
 
 $user_tags = array();
