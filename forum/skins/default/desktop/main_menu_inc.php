5452c3bde7c7d329f520a8e3697a69930f3c134b
Programmizd 02 <programmizd02@gmail.com>
2024-07-14 22:21:35 +0200

    Сделано отображение привилегированного статуса

--- forum/skins/default/desktop/main_menu_inc.php
+++ forum/skins/default/desktop/main_menu_inc.php
@@ -40,6 +40,11 @@ if($fmanager->get_user_name() == "admin")
 else
   $member_link = "<a class='member_nick' href='view_profile.php?uid=" . $fmanager->get_user_id() . "'>" . escape_html($fmanager->get_status_user_name()) . "</a>";
 
+if(!empty($_SESSION["privileged"]))
+{
+  $member_link .= "<img class='privileged_user' src='" . $view_path . "images/privilege.png' alt='" . escape_html(text("PrivilegedMember")) . "' title='" . escape_html(text("PrivilegedMember")) . "'>";
+}
+
 echo($member_link); 
 
 if (!empty($_SESSION["guest_posting_mode"])) {
