79f897f787dab87ec34e14a244de8ca193ee0d7a
programmizd02 <programmizd02@email.com>
2026-09-20 16:49:37 +0200

    small adjustments

--- installation/defaults/include/config_inc.php
+++ installation/defaults/include/config_inc.php
@@ -20,6 +20,18 @@ $SUPPORTED_DATABASES = array(
   "MSSQL" => "Microsoft SQL Server 2005+"
 );
 
+$SUPPORTED_AI = array(
+  "ChatGPT",
+  "Claude",
+  "Gemini",
+  "DeepSeek",
+  "Grok",
+  "GigaChat",
+  "Алиса"
+);
+
+asort($SUPPORTED_AI, SORT_LOCALE_STRING);
+
 $SUPPORTED_CODES = array(
     "apache" => "Apache",
     "nginx" => "Nginx",
@@ -123,9 +135,17 @@ define('WAIT_TIME_AFTER_ATTACK', 10); // minutes
 define('JOB_PER_CRON', 0);
 define('REFRESH_TOR_IPS', 1);
 
+define('RANDOM_AVATARS', 0);
+
 define('BULK_DELETE_COUNT', 5);
 
 define('MAX_SEARCH_RESULTS', 10000);
+
+// GeoIP keys
+
+define('GETGEOAPI_API_LANGUAGE', 'ru');
+define('GETGEOAPI_API_KEYS', [
+]);
 
 // The keys and token for retrival of the video meta data
 
@@ -166,6 +186,7 @@ define('TRACE_ERROR_VARS', false);
 define('TRACE_ERROR_ARGS', false);
 define('TRACE_STACK', false);
 define('MAIL_TO_TRACE', false);
+define('OMIT_FILE_NAMES_AND_LINES', false);
 
 // SMTP settings
 
